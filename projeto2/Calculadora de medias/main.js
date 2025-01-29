d204108d546ef1d010b3818372a092c9f24eb169
gui.gutinks@gmail.com <gui.gutinks@gmail.com>
2025-01-29 15:54:05 -0300

    project 2 implemented

---
+++ projeto2/Calculadora de medias/main.js
@@ -0,0 +1,78 @@
+const nomeAtv = document.getElementById('nomeAtv');
+const notaAtv = document.getElementById('notaAtv');
+const form = document.getElementById('form');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+
+const imgAprovado = '<img src="./style/images/aprovado.png" alt="emoji festejando">'
+const imgReprovado = '<img src="./style/images/reprovado.png" alt="emoji triste">'
+
+let notas = [];
+let atividades = [];
+
+
+
+function AdicionarAtividade(){
+    let linha = `<tr>`;
+    linha += `<td> ${nomeAtv.value}</td>`;
+    linha += `<td> ${notaAtv.value}</td>`;
+    linha += `<td> ${notaAtv.value >= 7 ? imgAprovado : imgReprovado} </td>`
+    linha += `</tr>`;
+    if(atividades.includes(nomeAtv.value)){
+        alert('esta atividade já foi inserida');
+    }else{
+        notas.push(parseInt(notaAtv.value));
+        atividades.push(nomeAtv.value);
+        tbody.innerHTML += linha;
+    }
+}
+
+function CalculaMedia(){
+    
+    let i = 0;
+    let somaNotas = 0;
+    for(i = 0; i < notas.length; i++){
+        somaNotas += notas[i];
+    }
+    let media = somaNotas/(notas.length);
+    return media;
+    
+}
+
+function EscreveMedia(media = 0){
+    let linha = `<tr>`;
+    linha += `<td>Média final</td>`;
+    linha += `<td>${media}</td>`;
+    linha += `<td> <span class= "resultado">${media >= 7 ? 'Aprovado': 'Reprovado'}</span></td>`;
+    linha += `</tr>`;
+    tfoot.innerHTML = linha;
+}
+
+function EstilizaAprovadoReprovado(){
+    let tfAprovado = document.querySelector('.resultado');
+    let media = CalculaMedia();
+    if(media >= 7){
+        tfAprovado.classList.add('aprovado');
+    } else {
+        tfAprovado.classList.add('reprovado');
+    }
+}
+//function calculaMedia(){
+    //let linhaAtual = linha;
+    //let numLinhas = linhaAtual.slice(linhaAtual.indexOf('-')+1, linhaAtual.indexOf('-')+2);
+    //alert(`numlinhas: ${numLinhas} tipo: ${typeof numLinhas}`);
+    //numLinhas = parseInt(numLinhas);
+    //alert(`numlinhas: ${numLinhas} tipo: ${typeof numLinhas}`);
+    //let notaTotal = notaAtv.value;
+    //for(let j = 0; j> numLinhas; j++){
+        //notaTotal += notas[j];
+    //}
+//}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    AdicionarAtividade();
+    let media = CalculaMedia();
+    EscreveMedia(media);
+    EstilizaAprovadoReprovado();
+});
