cae21ccc25ca17c51d8ac27a920b8c0826a039c6
gui.gutinks@gmail.com <gui.gutinks@gmail.com>
2025-03-17 17:01:32 -0300

    configurated build command

--- projeto4/classMaterial/gulpfile.js
+++ projeto4/classMaterial/gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const { src, dest } = require('gulp');
 const imagemin = require('gulp-imagemin')
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const replace = require('gulp-replace');
+const terser = require('gulp-terser')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -18,7 +21,19 @@ function images(){
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+function replaceHTML(){
+    return src('./src/*.html')
+        .pipe(replace('../dist', '.'))
+        .pipe(dest('./dist'));
+}
+
+function compressJS(){
+    return src('./src/scripts/*.js')
+        .pipe(terser())
+        .pipe(dest('./dist/scripts'));
+}
+
+exports.default = gulp.parallel(styles, images, replaceHTML, compressJS);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles));
 }
