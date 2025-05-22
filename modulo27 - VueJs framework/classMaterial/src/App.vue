1855859fcf317165127e1665c7427d889caa50a4
Guilherme Augusto <gui.gutinks@gmail.com>
2025-05-22 18:09:36 -0400

    finished all classes before the project

--- modulo27 - VueJs framework/classMaterial/src/App.vue
+++ modulo27 - VueJs framework/classMaterial/src/App.vue
@@ -1,47 +1,126 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const nome = 'guilherme augusto';
+//NAO PODE:
+//document.querySelector('h1').innerHTML = nome;
+const meuObj = {
+  nome: 'Guilherme',
+  filmeFav: 'Coraline'
+}
+
+function dizOla(nome) {
+  return `${nome} diz ola para voce`
+}
+
+const USMImageLink = 'https://www.planocritico.com/wp-content/uploads/2024/06/ultimate-homem-aranha-vol-1-2024-plano-critico.jpg';
+const XMImageLink = 'https://static1.srcdn.com/wordpress/wp-content/uploads/wm/2024/06/ultimate-x-men-vs-reboot-cyclops.jpg';
+const botaoEstaDesabilitado = false;
+let gostaDoUSM = true;
+let gostaDosXM = true;
+
+const estaAutorizado = true;
+
+//let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: '',
+  saldo: 5000,
+  transferindo: 0,
+  nomes: ['Guilherme', 'Leonam', 'Nagashi'],
+  nomeAInserir:'',
+});
+
+function incrementar(){
+  estado.contador++;
+}
+
+function decrementar(){
+  estado.contador--;
+}
+
+const alteraEmail = evento => estado.email = evento.target.value;
+const defineTransferencia = evento => estado.transferindo = evento.target.value;
+
+function mostraSaldoFuturo(){
+  const {saldo, transferindo} = estado;
+  return saldo - transferindo;
+}
+
+function validaTransferencia(){
+  const {saldo, transferindo} = estado;
+  return saldo >= transferindo;
+}
+
+const adicionaNome = evento => estado.nomeAInserir = evento.target.value; 
+
+function cadastrarNome(){
+  const {nomeAInserir, nomes}=estado;
+  if(nomeAInserir.length >= 3){
+    nomes.push(nomeAInserir)
+  } else{
+    alert('nome curto demais.')
+  }
+}
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- usar dessas mustaches para fazer o DOM ({{  }}) -->
+  <h1>{{dizOla('Lucas')}}</h1>
+  <!-- tomar cuidado em deixar tudo junto pra nao dar conflito -->
+  <img v-if="gostaDoUSM" :src="USMImageLink" alt="">
+  <img v-else-if="gostaDosXM" :src="XMImageLink" alt="">
+  <h2 v-else>Nao gosta dos mais famosos do novo ultiverse</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1 v-if="estaAutorizado">Seja Bem Vindo!!</h1>
+  <h1 v-else>Nao possui acesso</h1>
+  <button :disabled="botaoEstaDesabilitado">enviar mensagem</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <br>
+  <hr>
+
+  {{ estado.contador }}
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br>
+  <hr>
+
+  {{ estado.email }}
+  <input type="email" @keyup="alteraEmail">
+
+  <br>
+  <hr>
+
+  Saldo: {{ estado.saldo }} <br>
+  Transferindo: {{ estado.transferindo }} <br>
+  Saldo depois da transferencia: {{ mostraSaldoFuturo() }} <br>
+  <input :class="{invalido: !validaTransferencia()}" @keyup="defineTransferencia" type="number" placeholder="quantia para transferir"> <br>
+  <button v-if="validaTransferencia()">Transferir</button>
+  <span v-else>valor Invalido: o valor a transferir esta maior que o saldo atual</span>
+
+  <br>
+  <hr>
+
+  <ul>
+    <li v-for="nome in estado.nomes">
+      {{ nome }}
+    </li>
+  </ul>
+  <input @keyup="adicionaNome" type="text" placeholder="Digite um nome para adicionar a lista">
+  <button @click="cadastrarNome()" type="button">Cadastrar nome</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+img{
+  max-width: 500px;
+  width: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.invalido{
+  outline-color: red;
+  border-color: red;
 }
 </style>
