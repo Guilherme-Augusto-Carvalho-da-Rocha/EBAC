48056f817feac73d00077df9db7271d5f363bf4f
Guilherme Augusto <gui.gutinks@gmail.com>
2025-05-29 17:08:13 -0400

    estruturando o projeto

---
+++ modulo28 - React/activity/src/App.jsx
@@ -0,0 +1,14 @@
+import Calculator from "./components/Calculator"
+import Result from "./components/Result"
+
+//IMC = peso * pow(altura,2)
+function App() {
+  return(
+    <>
+    <Calculator/>
+    <Result/>
+    </>
+  )
+}
+
+export default App
---
+++ modulo28 - React/activity/src/components/Result/index.jsx
@@ -0,0 +1,9 @@
+//vai ser dividido entre: IMC: 00 e Classificacao: XXXXXXXX
+const Result = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Result;
