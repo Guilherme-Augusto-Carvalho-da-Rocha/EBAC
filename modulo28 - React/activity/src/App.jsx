e54683825fd64151fde7f29c208616b00232e92c
Guilherme Augusto <gui.gutinks@gmail.com>
2025-05-30 15:11:16 -0400

    logica do projeto feita

--- modulo28 - React/activity/src/App.jsx
+++ modulo28 - React/activity/src/App.jsx
@@ -1,12 +1,20 @@
 import Calculator from "./components/Calculator"
 import Result from "./components/Result"
+import { useState } from "react"
 
 //IMC = peso * pow(altura,2)
 function App() {
+  const [iMC, setIMC] = useState(0);
+  const [calculado, setCalculado] = useState(false);
+  const [abaixoPeso, setAbaixoPeso] = useState(false);
+  const [pesoNormal, setPesoNormal] = useState(false);
+  const [sobrepeso, setSobrepeso] = useState(false);
+  const [obeso, setObeso] = useState(false);
   return(
     <>
-    <Calculator/>
-    <Result/>
+    <Calculator setIMC={setIMC} setPesoNormal={setPesoNormal} setAbaixoPeso={setAbaixoPeso} setSobrepeso={setSobrepeso} 
+    setObeso={setObeso} setCalculado={setCalculado}/>
+    <Result iMC={iMC} calculado={calculado} abaixoPeso={abaixoPeso} obeso={obeso} pesoNormal={pesoNormal} sobrepeso={sobrepeso}/>
     </>
   )
 }
